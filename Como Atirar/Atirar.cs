c174ae8f29c75007768ad0ff309948ddd386f171
Agilis <78825042+AgilisDev@users.noreply.github.com>
2021-05-31 11:01:14 -0300

    Add files via upload

---
+++ Como Atirar/Atirar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atirar : MonoBehaviour
+{
+    public GameObject Bala; 
+
+    public void atirar()
+    {
+        Instantiate(Bala, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+}
---
+++ Como Atirar/MoveBala.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBala : MonoBehaviour
+{
+
+    public float velocidade;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2 (velocidade * Time.deltaTime, 0));
+        Destroy(this.gameObject, 5f);
+    }
+}
